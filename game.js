dd8cb290b0acfca6b7f6bc4ba156096248ab92de
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-13 21:23:14 -0400

    Adds button click listener

--- game.js
+++ game.js
@@ -4,25 +4,49 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
-let randomNumber;
-let randomChosenColor;
+let userClickedPattern = [];
 
 
 // ┌─────────────────┐
 // │   Main Program  │	
 // └─────────────────┘
-nextSequence();
-randomChosenColor = buttonColors[randomNumber];
-gamePattern.push(randomChosenColor);
-$(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
-const audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-audio.play();
 
 
 
 // ┌──────────────┐
 // │   Functions  │	
 // └──────────────┘
+$(".btn").click(function(){
+    
+    // Get the id of the button that was clicked
+    const userChosenColor = this.id;
+
+    // Save the color in the array
+    userClickedPattern.push(userChosenColor);
+
+    // Play the sound of the color
+    playSound(userChosenColor);
+})
+
 function nextSequence() {
-    randomNumber = Math.floor(Math.random() * 4);
+    
+    // Get a random number
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    // Get a color
+    const randomChosenColor = buttonColors[randomNumber];
+
+    // Save the color in the array
+    gamePattern.push(randomChosenColor);
+
+    // Make the button flash
+    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+
+    // Play the sound of the color
+    playSound(randomChosenColor);
+}
+
+function playSound(color){
+    const audio = new Audio(`sounds/${color}.mp3`);
+    audio.play();
 }
